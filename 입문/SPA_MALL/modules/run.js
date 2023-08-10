08210c78490ae79f0e6983832c71654730acc35c
Rio-323 <kimsiyeon5214@gmail.com>
2023-08-10 17:12:56 +0900

    module의 이해

--- 입문/SPA_MALL/modules/run.js
+++ 입문/SPA_MALL/modules/run.js
@@ -1,4 +1,7 @@
-const add = require("./math.js");
+// const add = require("./math.js");
+const {add} = require("./math.js");
 
 // console.log(add(10, 30));
-console.log(add.add(10, 30)); // 객체로 내보내준다.
+// console.log(add.add(10, 30)); // 객체로 내보내준다.
+
+console.log(add(10, 30));
