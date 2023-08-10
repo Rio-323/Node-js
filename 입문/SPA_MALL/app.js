542f6fd755394e484403769b424c5d7ac8731445
Rio-323 <kimsiyeon5214@gmail.com>
2023-08-10 16:49:24 +0900

    Routing 이해 및 Router 학습

--- 입문/SPA_MALL/app.js
+++ 입문/SPA_MALL/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const goodsRouter = require('./routes/goods.js');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// localhost:3000/api/~
+app.use("/api", goodsRouter);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
